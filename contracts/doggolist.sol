fb29ab0f67643cce5eb58d4037ce5a1736a97b22
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-11-19 20:13:55 -0300

    Reorder methods.

--- contracts/doggolist.sol
+++ contracts/doggolist.sol
@@ -8,6 +8,10 @@ contract DoggoList {
     mapping (uint32 => Doggo) data;
     uint32 length;
 
+    constructor() public {
+        length = 0;
+    }
+
     function push(Doggo _doggo) public {
         data[length] = _doggo;
         length += 1;
@@ -15,9 +19,5 @@ contract DoggoList {
 
     function isEmpty() public view returns (bool) {
         return length == 0;
-    }
-
-    constructor() public {
-        length = 0;
     }
 }
