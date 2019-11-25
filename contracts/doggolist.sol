1285c9f26c465bf11ebd376714f174da947aa8bc
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-11-25 16:07:59 -0300

    Switch to signed integer.

--- contracts/doggolist.sol
+++ contracts/doggolist.sol
@@ -5,8 +5,8 @@ import {Doggo} from './doggo.sol';
 // Because Solidity has no struct copy from memory to storage, so manual
 // array implementation is needed.
 contract DoggoList {
-    mapping (uint32 => Doggo) private data;
-    uint32 private _length;
+    mapping (int => Doggo) private data;
+    int private _length;
 
     constructor() public {
         _length = 0;
@@ -22,13 +22,13 @@ contract DoggoList {
         return data[_length + 1];
     }
 
-    function at(uint32 index) public view returns(Doggo) {
-        require(index < _length, 'Index must be in list bounds.');
+    function at(int index) public view returns(Doggo) {
+        require(index >= 0 && index < _length, 'Index must be in list bounds.');
 
         return data[index];
     }
 
-    function length() public view returns(uint32) {
+    function length() public view returns(int) {
         return _length;
     }
 
